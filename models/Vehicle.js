c884f739d09837a1054e59f846ff8c1fe50cd68d
gmdarient <gmedina@darient.com>
2025-01-26 10:38:57 -0400

    añadiendo schema vehiculos

---
+++ models/Vehicle.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const VehicleSchema = new mongoose.Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['disponible', 'en mantenimiento', 'en servicio'],
+      default: 'disponible',
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+const Vehicle = mongoose.model('Vehicle', VehicleSchema);
+module.exports = Vehicle;
